505e0394791674d9f8e6ce3a0097aacf1d91f05f
Maharshi <joshimaharshi123@gmail.com>
2025-08-25 23:37:57 +0530

    Create users model and add create table file

---
+++ app/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set in .env file")
+
+engine = create_engine(DATABASE_URL)
+session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
